b35c217ec2e7c9154762f14bd1cd41f4e37e78e5
Roman Klimenko <roman@klimenko.dk>
2025-11-28 12:27:31 +0100

    fix: correct hidden files setting and improve tab cleanup logic
    - Set hidden = false to not show hidden files by default (only git-ignored files are hidden)
    - Rewrite tab cleanup to close the original empty tab after opening file in new tab
    - Remove unnecessary <CR> mapping override
    - Improve cleanup timing and reliability

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,26 +10,28 @@ return {
       local actions = require("telescope.actions")
       local action_state = require("telescope.actions.state")
 
-      -- Helper to close empty unnamed buffers after opening a file
-      local function close_empty_unnamed_buffers()
-        local buffers = vim.api.nvim_list_bufs()
-        for _, buf in ipairs(buffers) do
-          if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
-            local name = vim.api.nvim_buf_get_name(buf)
-            local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-            local is_empty = #lines == 1 and lines[1] == ""
-            if name == "" and is_empty and not vim.bo[buf].modified then
-              vim.api.nvim_buf_delete(buf, { force = false })
-            end
-          end
-        end
-      end
+      -- Custom tab select that cleans up empty starting buffer
+      local function select_tab_and_cleanup(prompt_bufnr)
+        -- Store current tab before opening new one
+        local current_tab = vim.api.nvim_get_current_tabpage()
+        local current_buf = vim.api.nvim_get_current_buf()
+        local buf_name = vim.api.nvim_buf_get_name(current_buf)
+        local buf_lines = vim.api.nvim_buf_get_lines(current_buf, 0, -1, false)
+        local is_empty = buf_name == "" and #buf_lines == 1 and buf_lines[1] == ""
 
-      -- Custom select action that cleans up empty buffers
-      local function select_and_cleanup(prompt_bufnr, mode)
-        local select_fn = mode == "tab" and actions.select_tab or actions.select_default
-        select_fn(prompt_bufnr)
-        vim.defer_fn(close_empty_unnamed_buffers, 10)
+        -- Open in new tab
+        actions.select_tab(prompt_bufnr)
+
+        -- If the original buffer was empty, close its tab
+        if is_empty and vim.api.nvim_tabpage_is_valid(current_tab) then
+          vim.defer_fn(function()
+            local current_new_tab = vim.api.nvim_get_current_tabpage()
+            if current_tab ~= current_new_tab then
+              vim.api.nvim_set_current_tabpage(current_tab)
+              vim.cmd("tabclose")
+            end
+          end, 50)
+        end
       end
 
       require("telescope").setup({
@@ -38,12 +40,7 @@ return {
             i = {
               ["<C-j>"] = "move_selection_next",
               ["<C-k>"] = "move_selection_previous",
-              ["<C-t>"] = function(prompt_bufnr)
-                select_and_cleanup(prompt_bufnr, "tab")
-              end,
-              ["<CR>"] = function(prompt_bufnr)
-                select_and_cleanup(prompt_bufnr, "default")
-              end,
+              ["<C-t>"] = select_tab_and_cleanup,
             },
           },
           layout_config = {
@@ -55,7 +52,7 @@ return {
         },
         pickers = {
           find_files = {
-            hidden = true,
+            hidden = false,
             no_ignore = false,
           },
         },
