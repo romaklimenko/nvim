d54d48fc148205e5cb57c3843e984246dc32467a
Roman Klimenko <roman@klimenko.dk>
2025-11-28 12:14:58 +0100

    fix: remove telescope-file-browser autocmd
    Remove autocmd that opened file browser when opening directories.
    This autocmd was causing errors after removing the telescope-file-browser plugin.

--- lua/editor/options.lua
+++ lua/editor/options.lua
@@ -10,21 +10,6 @@ function M.setup()
   o.termguicolors = true
   o.splitright = true
   o.splitbelow = true
-
-  -- Open telescope-file-browser when opening a directory
-  vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function(data)
-      local is_directory = vim.fn.isdirectory(data.file) == 1
-      if is_directory then
-        vim.cmd.cd(data.file)
-        vim.defer_fn(function()
-          require("telescope").extensions.file_browser.file_browser({
-            cwd = data.file,
-          })
-        end, 0)
-      end
-    end,
-  })
 end
 
 return M
